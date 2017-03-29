2dd74f1220b7aa34f25dfe6efc4a71708262390e
a91566.Xiaobao <a91566@163.com>
2017-03-29 21:36:16 +0800

    春雨下不停

---
+++ CCTextBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace wfa_20170329_CCTextBox
+{
+	public partial class CCTextBox : TextBox, ICCControlGetValue
+	{
+		public CCTextBox()
+		{
+			InitializeComponent();
+		}
+
+		public CCTextBox(IContainer container)
+		{
+			container.Add(this);
+			InitializeComponent();
+		}
+
+		[Description("格式化")]
+		public string ccFormat { get; set; }
+
+		public T GetValue<T>()
+		{
+			if (ccFormat == "0.01d")
+			{
+				Text = Convert.ToDouble(Text).ToString("0.00");
+			}
+			return (T)Convert.ChangeType(Text, typeof(T));
+		}
+
+		
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfa_20170329_CCTextBox
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string temp = this.ccTextBox1.GetValue<string>();
+			MessageBox.Show(temp);
+			
+		}
+	}
+}
